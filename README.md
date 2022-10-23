805a07918a45741381f8d513fb3dfe031f9673f8
GustavoMB <36745850+GustavoMB0@users.noreply.github.com>
2022-10-23 15:18:44 -0300

    criação

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# assistirJuntp
+# assistirJunto
  software para assistir videos com amigos
+
+Software feito utilizando nodeJs e ReactJs
+Para executar esse projeto abra um terminal na pasta raiz e rode o comando 
+##**npm install**
+depois execute o comando ##**npm start** para iniciar o servidor.
+
+Com o servidor rodando abra a pasta watchtogethere execute novamente o comando
+##**npm install** 
+segudio por ##**npm start**
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require(express);
+const path = require(path);
+const socket = require(socket.io);
+const http = require(http);
+const app = express();
+const server = http.creatServer(app);
+const io = socket(server);
+
+const {ExpressPeerServer} = require(peer).ExpressPeerServer;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/peerjs', ExpressPeerServer)
+
+io.on('connection', socket => {
+    socket.on('join', (id) => {
+        const user = userJoin(socket.id, room);
+        socket.join(room);
+    });
+});
+
+io.on('joined', (id, userId) => {
+    socket.to(id).broadcast.emit('user connected', userId);
+
+    socket.on('disconect', () => {
+        const user = leave(socket.id);
+
+        io.to(user.room).emit('users', {
+            room: user.room,
+            users: getUsers(user.room)
+        });
+
+    });
+});
+
+socket.on('play', ({method, ctime}) =>{
+    const user = getUser(socket.id);
+    io.to(user.room).emit('play', {method, ctime});
+});
+
+socket.on('pause', ({method, ctime}) => {
+    const unser = getUser(socket.id);
+    io.to(user.room).emit('pause', {method, ctime});
+})
+
+
+sockt.on('foward_backward', (time) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('client_fb', {time});
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log('server runing on port ${PORT}'));
